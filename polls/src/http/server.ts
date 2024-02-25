c77c0acf194b728e55586fc1f845cd4397e4d647
Letícia <leticia.c.rodrigues0@gmail.com>
2024-02-25 10:17:43 -0300

    Node - Fim aula 3

--- polls/src/http/server.ts
+++ polls/src/http/server.ts
@@ -1,27 +1,29 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 import { PrismaClient} from '@prisma/client'
-import { z } from 'zod'
+import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
+import { voteOnPoll } from './routes/vote-on-poll'
+import websocket from '@fastify/websocket'
+import { pollResults } from './ws/poll-results'
+
 
 const app = fastify()
 
 const prisma = new PrismaClient()
 
 
-app.post('/polls', async (request, reply) => {
-    const createPollBody = z.object({
-        title: z.string()
-    })
-
-    const { title } = createPollBody.parse(request.body)
-
-    const poll = await prisma.poll.create({
-        data: {
-            title,
-        }
-    })
-
-    return reply.status(201).send({pollId: poll.id})
+app.register(cookie, {
+    secret: "my-secret", 
+    hook: 'onRequest', 
 })
+
+app.register(websocket)
+
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
+app.register(pollResults)
 
 app.listen({ port: 3333}).then(() =>{
     console.log('HTTP server running!');
